985b9e644723aefe8374e55b148540811ab286d8
Taro <>
2018-08-02 01:42:21 +0900

    テキスト追加。じゃんけん時の入力をIntからStringに(見やすいように)。

--- JankenApp/ViewController.swift
+++ JankenApp/ViewController.swift
@@ -11,59 +11,50 @@ import GameplayKit
 
 class ViewController: UIViewController {
     
+    //乱数作成インスタンス
     let randomJanken = GKRandomSource()
 
-    
+    //テキストとじゃんけん画像のインポート
     @IBOutlet weak var text1: UILabel!
-    
-    
     @IBOutlet weak var jankenImage: UIImageView!
     
     
-    @IBAction func gu(_ sender: Any) {
-        jankenplay(Player: 0)
+    
+    //じゃんけん実行メソッド
+    //引数[0 = グー],[1 = チョキ],[2 = パー]
+    func jankenplay(Player: String){
         
-        
-    }
-    
-    
-    @IBAction func choki(_ sender: Any) {
-         jankenplay(Player: 1)
-    }
-    
-    
-    @IBAction func pha(_ sender: Any) {
-         jankenplay(Player: 2)
-    }
-    
-    
-    func jankenplay(Player: Int){
+        //1~3までの乱数生成
         let comp = randomJanken.nextInt(upperBound: 3)
+        //じゃんけんメッセージ
         var msg = ""
+        
+        
         switch comp {
+            //乱数0の時
         case 0:
             jankenImage.image = UIImage(named: "M-j_gu01.png")
             switch Player{
-                
-            case 0:
+            case "グー": //グー
                 msg = "あいこじゃ"
-            case 1:
+            case "チョキ": //チョキ
                 msg = "おぬしの負けじゃ"
-            case 2:
+            case "パー": //パー
                 msg = "おぬしの勝ちじゃ"
             default:
                 break
                 
             }
+            //乱数1の時
         case 1:
             jankenImage.image = UIImage(named: "M-j_ch01.png")
             switch Player{
                 
-            case 0:
+            case "グー": //グー
                 msg = "おぬしの勝ちじゃ"
-            case 1:
+            case "チョキ": //チョキ
                 msg = "あいこじゃ"
-            case 2:
+            case "パー": //パー
                 msg = "おぬしの負けじゃ"
             default:
                 break
@@ -73,11 +64,11 @@ class ViewController: UIViewController {
             jankenImage.image = UIImage(named: "M-j_pa01.png")
             switch Player{
                 
-            case 0:
+            case "グー": //グー
                 msg = "おぬしの負けじゃ"
-            case 1:
+            case "チョキ": //チョキ
                 msg = "おぬしの勝ちじゃ"
-            case 2:
+            case "パー": //パー
                 msg = "あいこじゃ"
             default:
                 break
@@ -90,7 +81,20 @@ class ViewController: UIViewController {
         
     }
     
+    //グーボタンが押された時の処理
+    @IBAction func gu(_ sender: Any) {
+        jankenplay(Player: "グー")
+    }
     
+    //チョキボタンが押された時の処理
+    @IBAction func choki(_ sender: Any) {
+        jankenplay(Player: "チョキ")
+    }
+    
+    //パーボタンが押された時の処理
+    @IBAction func pha(_ sender: Any) {
+        jankenplay(Player: "パー")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
